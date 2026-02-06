20b9ab514f031d5cf1c3d821160d4fb5a4ebb1dd
Maxence <max.prrn@proton.me>
2026-02-06 18:21:32 -0500

    Sound + manager

---
+++ JamRubikaMontreal/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    
+    void Start()
+    {
+        Play("treadmill1");
+        Play("treadmill2");
+        Play("treadmill3");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.outputAudioMixerGroup = s.audioMixerGroup;
+        s.source.clip = s.clip;
+        s.source.loop = s.loop;
+        s.source.volume = s.volume;
+        s.source.pitch = s.pitch;
+        s.source.Play();
+    }
+
+    public void Stop()
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Stop();
+    }
+}
